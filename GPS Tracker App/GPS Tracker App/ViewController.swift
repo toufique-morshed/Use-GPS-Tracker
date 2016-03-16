8c01cbcf9a2d6ddb519d738aca7334067cd431ef
morshed <t.morshed1370@gmail.com>
2016-03-16 12:13:53 +0600

    gps tracker check completed

--- GPS Tracker App/GPS Tracker App/ViewController.swift
+++ GPS Tracker App/GPS Tracker App/ViewController.swift
@@ -7,17 +7,59 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    //MARK: LOcation Properties
+    
+    let locationManager = CLLocationManager()
+    
+    //MARK: UI Properties
+    @IBOutlet weak var positionLabel: UILabel!
+    @IBOutlet weak var positionFinderButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        positionLabel.text = "find your position hitting the button"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK: CLLocation Manager delegate methods
+    func locationManager(manager: CLLocationManager, rangingBeaconsDidFailForRegion region: CLBeaconRegion, withError error: NSError) {
+        positionLabel.text = error.description
+        
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("I am here in the manager")
+        CLGeocoder().reverseGeocodeLocation(manager.location!, completionHandler: {(placemarks,error) in
+            print("in completion handler")
+            if let error = error {
+                self.positionLabel.text = error.localizedDescription
+            } else if placemarks?.count > 0 {
+                let pm = placemarks![0] as CLPlacemark
+                self.positionLabel.text = pm.locality
+                
+            }
+        })
+        
+    }
+    
+    //MARK: position Finder method
+    @IBAction func positionFinder(sender: AnyObject) {
+        print("button Pressed")
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        
     }
 
 
